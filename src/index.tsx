f6e4c081292b308a38b1b653fb4dd64c0fff661d
Brian Gracie <7625540+bgracie@users.noreply.github.com>
2020-01-09 16:25:44 -0500

    Remove unused service worker module

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import * as serviceWorker from "./interface/service_worker";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { AppContainer } from "./view/AppContainer";
@@ -16,8 +15,3 @@ ReactDOM.render(
   <AppContainer store={store} />,
   document.getElementById("root") as HTMLElement
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
